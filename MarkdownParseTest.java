31fcd195d8b7c7425b987f83484abe73799cb481
qzou1215 <zouqt.christine2018@gdhfi.com>
2022-02-10 18:59:56 -0800

    Create MarkdownParseTest.java

---
+++ MarkdownParseTest.java
@@ -0,0 +1,43 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class MarkdownParseTest {
+    @Test
+    public void addition() {
+        assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testFile1() throws IOException {
+        String contents= Files.readString(Path.of("./test-file.md"));
+        List<String> expect = List.of("https://something.com", "some-page.html");
+        assertEquals(MarkdownParse.getLinks(contents), expect);
+    }
+
+    @Test
+    public void testFile2() throws IOException {
+        String contents= Files.readString(Path.of("./test-file2.md"));
+        List<String> expect = List.of("https://something.com", "some-page.html");
+        assertEquals(MarkdownParse.getLinks(contents), expect);
+    }
+
+    @Test
+    public void testFile3() throws IOException {
+        String contents= Files.readString(Path.of("./test-file3.md"));
+        List<String> expect = List.of();
+        assertEquals(MarkdownParse.getLinks(contents), expect);
+    }
+
+    @Test
+    public void testFile8() throws IOException {
+        String contents= Files.readString(Path.of("./test-file8.md"));
+        List<String> expect = List.of("some-page.html");
+        assertEquals(MarkdownParse.getLinks(contents), expect);
+    }
+    
+}
